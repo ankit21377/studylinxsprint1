7d9754e2dfde2d484d480360a173d0b8a1194c14
ankit21377 <ankitkharel678@gmail.com>
2025-12-29 15:10:49 +0545

    notification screen start

--- app/src/main/java/com/example/studylinx/NotificationScreen.kt
+++ app/src/main/java/com/example/studylinx/NotificationScreen.kt
@@ -1,19 +1,29 @@
 package com.example.studylinx
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 @Composable
 fun NotificationScreen(){
     Column(
-        modifier = Modifier.fillMaxSize().background(Color.White)
+        modifier = Modifier.fillMaxSize().background(Color.White),
+
     ) {
 
-        Text("Notification Screen")
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){Text("Notification Screen")
+    }
     }
 }
