0b241f8514a0e790357b81784c52e351f4076eb2
ankit21377 <ankitkharel678@gmail.com>
2026-01-11 15:11:45 +0545

    making appointment repositary

---
+++ app/src/main/java/com/example/studylinx/repo/AppointmentRepo.kt
@@ -0,0 +1,14 @@
+package com.example.studylinx.repo
+
+import com.example.studylinx.model.Appointment
+import kotlinx.coroutines.flow.Flow
+
+interface AppointmentRepository {
+    fun streamAppointmentsInRange(
+        userId: String,
+        startMillisInclusive: Long,
+        endMillisExclusive: Long
+    ): Flow<List<Appointment>>
+
+    suspend fun addAppointment(userId: String, appointment: Appointment): String
+}
