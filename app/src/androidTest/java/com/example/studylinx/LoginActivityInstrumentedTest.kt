6a721398bcb79bb6e494b5407a93a369cc85befd
Saurav mainali <sauravmainali83@gmail.com>
2026-01-28 13:03:12 +0545

    Added instrumented test for email and password

--- app/src/androidTest/java/com/example/studylinx/LoginActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/studylinx/LoginActivityInstrumentedTest.kt
@@ -1,4 +1,25 @@
 package com.example.studylinx
 
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class LoginActivityInstrumentedTest {
+
+    @get:Rule
+    val rule = createAndroidComposeRule<LoginActivity>()
+
+    @Test
+    fun loginScreen_displaysEmailField() {
+        rule.onNodeWithTag("email").assertExists()
+    }
+    @Test
+    fun loginScreen_displaysPasswordField() {
+        rule.onNodeWithTag("password").assertExists()
+    }
+
 }
